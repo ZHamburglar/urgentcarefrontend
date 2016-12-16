ad7e732e019db7bdfc8f81090241a2d4ca8cd98e
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2016-12-16 12:25:31 -0600

    added dropdown

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-alert('hello my dude!')
+$(document).ready(function(){
+  // $('.board td').on('click', placePiece)
+  // $('td').on('click', findWin)
+  $('#butan').on('click', Hello)
+
+
+});
+
+
+function Hello(){
+  alert('Hello there!')
+}
