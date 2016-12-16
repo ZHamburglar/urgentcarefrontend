315207cfbdde164f9fa720d9c54d59c5842989f3
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2016-12-16 14:09:17 -0600

    added Auth0 button that works

--- js/main.js
+++ js/main.js
@@ -1,12 +1,47 @@
-$(document).ready(function(){
-  // $('.board td').on('click', placePiece)
-  // $('td').on('click', findWin)
-  $('#butan').on('click', Hello)
+
+var lock = new Auth0Lock('kCWx6N5YOzpdWxqzWVTjNmrf8EQ85qVA', 'thegoblinking.auth0.com', {
+    auth: {
+      params: {
+        scope: 'openid email'
+      }
+    }
+  });
+
+lock.on('authenticated', function (authResult) {
+  console.log('authResult', authResult);
+  localStorage.setItem('idToken', authResult.idToken)
+  showProfile()
+})
+
+$(document).ready(function() {
+  $('#btn-login').on('click', function (e) {
+    e.preventDefault()
+    lock.show()
+  })
+
+  $('#btn-logout').on('click', function (e) {
+    e.preventDefault()
+    logout()
+  })
 
 
 });
 
 
-function Hello(){
-  alert('Hello there!')
+
+
+
+
+
+function isLoggedIn() {
+  if (localStorage.getItem('idToken')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function logout() {
+  localStorage.removeItem('idToken')
+  window.location.href = '/';
 }
